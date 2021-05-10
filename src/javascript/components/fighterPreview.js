e116d3a6c015e3d8a46105ca640028124fd77482
yaroslav207 <yaroslav.sprynsian8@gmail.com>
2021-05-10 19:41:38 +0300

    finish function createFighterPreview and create function createInfoWrapper for view info fighter

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,7 +7,14 @@ export function createFighterPreview(fighter, position) {
     className: `fighter-preview___root ${positionClassName}`,
   });
 
-  // todo: show fighter info (image, name, health, etc.)
+  if(!fighter){
+    return fighterElement
+  }
+  const image = createFighterImage(fighter)
+  const info = createElement({tagName: 'ul', className: 'fighter-preview___info'})
+  const infoItem = createInfoWrapper(fighter)
+  info.append(...infoItem)
+  fighterElement.append(image, info)
 
   return fighterElement;
 }
@@ -26,4 +33,14 @@ export function createFighterImage(fighter) {
   });
 
   return imgElement;
+}
+
+function createInfoWrapper(fighter) {
+  return Object.keys(fighter)
+      .filter(key => key !== '_id' && key !== 'source')
+      .map((key) => {
+        const infoItem = createElement({tagName: 'li', className: 'fighter-preview___info-item'})
+        infoItem.innerHTML = `${key}: ${fighter[key]}`
+        return infoItem
+      })
 }
