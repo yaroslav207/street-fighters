f318a4e15e931a35364031feb88250f7ca5227d5
yaroslav207 <yaroslav.sprynsian8@gmail.com>
2021-05-10 19:46:16 +0300

    create class Player

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -17,3 +17,37 @@ export function getHitPower(fighter) {
 export function getBlockPower(fighter) {
   // return block power
 }
+
+class Player {
+  constructor(playerInfo) {
+    this._id = playerInfo._id;
+    this.name = playerInfo.name;
+    this.attack = playerInfo.attack;
+    this.block = false;
+    this.source = playerInfo.source;
+    this.criticalHit = true;
+    this.criticalHitKeys = playerInfo.criticalHitCombination.reduce((obj, item) => (obj[item] = false, obj), {});
+    this.currentHealth = playerInfo.health;
+    this.defense = playerInfo.defense;
+    this.health = playerInfo.health;
+  }
+
+  get percentHealth() {
+    return (this.currentHealth / this.health) * 100
+  }
+
+  takeDamage(damage) {
+    if (this.currentHealth <= damage) {
+      this.currentHealth = 0
+    } else {
+      this.currentHealth = this.currentHealth - damage
+    }
+  }
+
+  coolDownCriticalHit() {
+    this.criticalHit = false;
+    setTimeout(() => {
+      this.criticalHit = true
+    }, 10000);
+  }
+}
